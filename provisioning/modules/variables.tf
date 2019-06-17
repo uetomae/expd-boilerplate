feaafdd5d7e1fdb3118d03df34fcef814298d131
Naohiko MORI <naohiko.mori@uetomae.co.jp>
2019-06-17 13:49:27 +0900

    modify default certification

--- provisioning/modules/variables.tf
+++ provisioning/modules/variables.tf
@@ -1,5 +1,5 @@
 variable "alb_certificate_arn" {
-  default = "arn:aws:acm:ap-northeast-1:891347377932:certificate/4420da30-17e5-4fb4-92ea-43dc57f63849"
+  default = "arn:aws:acm:ap-northeast-1:891347377932:certificate/581bc2bd-ecc4-499d-9e8e-b648d5d5ef91"
 }
 
 variable "app_name" {}
